2de96d907f78e7818c04c9b3fbe6807af0f681f7
Sarup Banskota <sbanskota08@gmail.com>
2014-09-10 22:29:07 +0530

    Make variable names follow conventions, use symbols where appropriate

--- schedlr.rb
+++ schedlr.rb
@@ -1,23 +1,22 @@
 # WIP 
 # Takes in a popularity stack: a bunch of conference proposals
 # sorted by popularity, and slots them in the schedule
-# Constraints: pre-defined free slots, varying durations 
+# Constraints: pre-defined break slots, varying durations 
 # (short and long in this example)
 
-$FREE = "Free"
-$TITLE = 0
-$LENGTH = 1
-$LONG = "long"
-$SHORT = "short"
-$LONG_SIZE = 2
-$SHORT_SIZE = 1
-$FLAG = 0
-$SLOT_LIMIT = 25
+TITLE = 0
+LENGTH = 1
+LONG_SIZE = 2  # factors to a unit
+SHORT_SIZE = 1 # of time, e.g 15mins
+SLOT_LIMIT = 25
+
+
+$flag = 0 # keeps track of next (possibly) available short slot
 
 def big_enough_for_long? session, slot_index, schedule
   index = slot_index
-  until index > slot_index+$LONG_SIZE-1 
-    if schedule[index] == $FREE
+  until index >= slot_index+LONG_SIZE
+    if schedule[index] == :break
       return false, index+1
     end
     index+=1
@@ -27,8 +26,8 @@ end
 
 def allot_long session, slot_index, schedule
   index = slot_index
-  until index > slot_index+$LONG_SIZE-1
-    schedule[index] = session[$TITLE]
+  until index >= slot_index+LONG_SIZE
+    schedule[index] = session[TITLE]
     index+=1
   end
   return index
@@ -36,14 +35,14 @@ end
 
 def assign session, slot_index, schedule
   if session == nil
-    schedule.push $FREE
+    schedule.push :break
     return slot_index+1
-  elsif session[$LENGTH] == $SHORT
-    index=$FLAG
-    until index > $SLOT_LIMIT
+  elsif session[LENGTH] == :short
+    index=$flag
+    until index > SLOT_LIMIT
       unless schedule[index]
-        schedule[index] = session[$TITLE]
-        $FLAG = index+1
+        schedule[index] = session[TITLE]
+        $flag = index+1
         break
       end
       index+=1
@@ -52,7 +51,7 @@ def assign session, slot_index, schedule
       return index+1
     end
     return slot_index
-  elsif session[$LENGTH] = $LONG
+  elsif session[LENGTH] = :long
     enough_capacity = big_enough_for_long? session, slot_index, schedule
     if enough_capacity[0]
       return allot_long(session, slot_index, schedule)
@@ -63,30 +62,30 @@ def assign session, slot_index, schedule
 end
 
 def schedule
-  # initialize schedule; also, add free slots
+  # initialize schedule; also, add break slots
   schedule = []
-  schedule[3] = "Free"
-  schedule[5]  = "Free"
-  schedule[9] = "Free"
-  schedule[11] = "Free"
+  schedule[3] = :break
+  schedule[5]  = :break
+  schedule[9] = :break
+  schedule[11] = :break
   popularity = [
-                  ["sarup","short"],
-                  ["jessica","short"], 
-                  ["gnokii", "long"],
-                  ["stacy", "short"],
-                  ["giraffe", "long"],
-                  ["piggy", "long"],
-                  ["laura", "long"],
-                  ["michael", "short"],
-                  ["puyol", "short"],
-                  ["joomla", "long"],
-                  ["isabelle", "short"]
+                  ["sarup",:short],
+                  ["jessica",:short], 
+                  ["gnokii", :long],
+                  ["stacy", :short],
+                  ["giraffe", :long],
+                  ["piggy", :long],
+                  ["laura", :long],
+                  ["michael", :short],
+                  ["puyol", :short],
+                  ["joomla", :long],
+                  ["isabelle", :short]
                ]
 
   slot_index = 0
 
-  until slot_index > $SLOT_LIMIT
-    if schedule[slot_index] == $FREE
+  until slot_index >= SLOT_LIMIT
+    if schedule[slot_index] == :break
       slot_index+=1
     else
       slot_index = assign popularity.pop, slot_index, schedule
@@ -97,5 +96,3 @@ end
 
 schedule()
 
-# TODO:
-# * Remove global variables, make methods to call
